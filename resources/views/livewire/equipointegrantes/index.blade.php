8804f739165cf7e65aa05cbfd40e0c30c3fa514b
Mateo M. <fmmedina3@espe.edu.ec>
2024-01-15 19:01:54 -0500

    Add line break in index.blade.php files

--- resources/views/livewire/equipointegrantes/index.blade.php
+++ resources/views/livewire/equipointegrantes/index.blade.php
@@ -2,6 +2,7 @@
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid">
+    <br>
     <div class="row justify-content-center">
         <div class="col-md-12">
             @livewire('equipointegrantes')
