c64459901617dd1830cd9ab0e9334bcd276fc0f9
JosueMorales <juniorbmorales@gmail.com>
2024-01-18 22:37:50 -0500

    edición de home

--- resources/views/vistas/nav.blade.php
+++ resources/views/vistas/nav.blade.php
@@ -19,22 +19,24 @@
         transform: scale(1.1);
     }
 
-    header {
-        background-color: #000000;
-background-image: url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%235531d0' fill-opacity='0.58' fill-rule='evenodd'/%3E%3C/svg%3E");
-    }
 </style>
 
+
 <!--<header x-data="{ isOpen: false }" class="bg-blue-900  shadow">-->
-<header x-data="{ isOpen: false }"  class="rounded-xl w-full h-full ">
-    <nav class="container mx-auto px-6 py-3">
+<header x-data="{ isOpen: false }"  class=" w-full h-full md:h-0  ">
+    <nav class="container mx-auto px-6 py-3  top-0 left-0 right-0">
         <div class="flex flex-col md:flex-row md:justify-between md:items-center">
-            <div class="flex justify-between items-center">
+            <div class="flex justify-around items-center">
                 <div class="flex items-center">
+<<<<<<< Updated upstream
                     <!--<a class="text-gray-800 text-xl font-bold md:text-2xl hover:text-gray-700" href="#">@GamerFest</a>-->
                     <img class="zoom-on-hover w-25 h-20 " src="{{ asset('img/logoGamerFest.png') }}" alt="GamerFest">
                    
                 
+=======
+                    
+                    <img class="zoom-on-hover size-28 " src="{{ asset('img/logoGamerFest.png') }}" alt="GamerFest">                
+>>>>>>> Stashed changes
                 </div>
                 <!-- Mobile menu button -->
                 <div class="flex md:hidden">
@@ -53,18 +55,18 @@ background-image: url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox=
             <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
             <div class="md:flex items-center" :class="isOpen ? 'block' : 'hidden'">
                 <div class="flex flex-col mt-2 md:flex-row md:mt-0 md:mx-1">
-                    <a class="my-1 text-lg text-white leading-5 hover:text-yellow-600 hover:underline md:mx-4 md:my-0 font-bold"
+                    <a class="my-1 text-lg text-white leading-5 hover:text-purple-600 hover:underline md:mx-4 md:my-0 font-bold"
                         href="#">Inicio</a>
-                    <a class="my-1 text-lg text-white leading-5 hover:text-yellow-600 hover:underline md:mx-4 md:my-0 font-bold"
+                    <a class="my-1 text-lg text-white leading-5 hover:text-purple-600  hover:underline md:mx-4 md:my-0 font-bold"
                         href="#">Juegos</a>
-                    <a class="my-1 text-lg text-white leading-5 hover:text-yellow-600 hover:underline md:mx-4 md:my-0 font-bold"
+                    <a class="my-1 text-lg text-white leading-5 hover:text-purple-600 hover:underline md:mx-4 md:my-0 font-bold"
                         href="#">Sobre Nosotros</a>
                 </div>
 
                 <div class="flex items-center py-2 -mx-1 md:mx-0">
-                    <a class="block w-1/2 px-3 py-2 mx-1 rounded-full text-center text-lg bg-gray-500 font-medium text-white leading-5 hover:bg-yellow-600 md:mx-2 md:w-auto "
+                    <a class="block w-1/2 px-3 py-2 mx-1 rounded-full text-center text-lg bg-purple-800 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-full transition-all duration-300 border-2 border-transparent hover:border-white focus:outline-none focus:ring focus:border-purple-300 font-medium text-white leading-5  md:mx-2 md:w-auto "
                         href="{{ route('login') }}">Ingresar</a>
-                    <a class="block w-1/2 px-3 py-2 mx-1 rounded-full  text-center text-lg bg-gray-500 font-medium text-white leading-5 hover:bg-yellow-600 md:mx-0 md:w-auto "
+                    <a class="block w-1/2 px-3 py-2 mx-1 rounded-full  text-center text-lg bg-purple-800 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-full transition-all duration-300 border-2 border-transparent hover:border-white focus:outline-none focus:ring focus:border-purple-300 font-medium text-white leading-5  md:mx-0 md:w-auto "
                         href="{{ route('register') }}">Registrarse</a>
                 </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,31 +10,100 @@
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
+    
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
-@component('vistas.nav')@endcomponent
 
-<body class="antialiased bg-black">
 
-    <!-- BIENVENIDA -->
-    <section class="flex items-center justify-center h-screen text-white bg-black">
-        <div class="p-8 text-center sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3">
-            <h1 class="text-4xl sm:text-5xl font-bold mb-4">Bienvenido al mejor evento de videojuegos del Ecuador</h1>
-            <p class="text-lg sm:text-2xl m-3">
-                Sumérgete en la emoción digital de los videojuegos y la competencia en línea.
-                <br>
-                <span class="font-extrabold text-3xl sm:text-5xl">INSCRÍBETE AHORA !!!</span>
-            </p>
-            <div class="flex justify-center mt-4 space-x-4">
-                <button class="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-full sm:w-1/2">VER MÁS</button>
-                <button class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-full sm:w-1/2 border-solid border-4 border-gray-600">INGRESAR</button>
-            </div>
+<body class="antialiased ">
+
+    
+   <section>
+    <div
+    class="relative overflow-hidden  bg-cover bg-center bg-no-repeat p-12 text-center bg-fixed"
+    style="background-image: url('img/bg_op1.jpg'); height: 750px ">
+    <div
+      class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+      style="background-color: rgba(0, 0, 0, 0.6)">
+      @component('vistas.nav')@endcomponent
+      <div class="flex h-full items-center justify-center">
+        <div class="text-white">
+          <h2 class="mb-4 text-4xl font-semibold">Bienvenido al mejor evento de videojuegos del Ecuador</h2>
+          <h4 class="mb-6 text-xl font-semibold">Sumérgete en la emoción digital de los videojuegos y la competencia en línea.
+              <br>
+              <span class="font-extrabold text-5xl sm:text-5xl ">INSCRÍBETE AHORA !!!</span></h4>
+              <button class="bg-purple-800 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-full transition-all duration-300 border-2 border-transparent hover:border-white focus:outline-none focus:ring focus:border-purple-300">
+                  INSCRIBIRSE
+               </button>
         </div>
-    </section>
+      </div>
+    </div>
+  </div>
+   </section>
 
-    <!-- JUEGOS -->
-    <section class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 content-center pt-8 m-4 text-white">
+<section class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 content-center pt-8 m-4">
+    @foreach ($juegos as $juego)
+    <div>
+        <div class="group 
+      overflow-hidden
+       relative shadow-lg max-w-xs">
+            <a href="#" class="absolute z-10 top-0 bottom-0 left-0 right-0"><a />
+                <img
+        class="block group-hover:opacity-40 transition-opacity duration-700"
+        src="https://cdn.cloudflare.steamstatic.com/steam/apps/230410/hero_capsule.jpg?t=1637183731"
+        />
+                <div
+                    class="absolute bg-black flex items-center group-hover:-top-0 group-hover:opacity-100 duration-700 top-full right-0 w-full opacity-0 h-1/2 transition-all">
+                    <div class=""
+                        style="background-image: url(&quot;https://cdn.cloudflare.steamstatic.com/steam/apps/230410/ss_2d79448091149a8cc790b62e7422615a011d015a.600x338.jpg?t=1637183731&quot;);">
+
+                        <div class="w-full aspect-w-16 aspect-h-9">
+                            <iframe class="w-full h-full" src="https://www.youtube.com/embed/6Mtfo8asqjM?autoplay=1&loop=1&mute=1&controls=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                          </div>
+                       
+                    </div>
+                </div>
+                <div class="absolute  bg-gradient-to-br duration-700 from-green-800 to-blue-800 text-white block left-0 right-0 top-full text-base h-1/2 w-full opacity-50 
+        transition-all group-hover:top-1/2 group-hover:opacity-100">
+                    <div class="py-4 text-xs px-7">
+                        <div class="text-xl font-bold">{{ $juego->nombre }}</div>
+                        <div class="overflow-ellipsis overflow-hidden whitespace-nowrap">
+                            <span class="uppercase text-gray-400 whitespace-nowrap text-xs md:text-sm">Developer:</span>
+                            <span class="whitespace-nowrap overflow-hidden overflow-ellipsis relative z-20">
+                                                        <a href="https://store.steampowered.com/developer/DigitalExtremes?snr=1_4_600__629">Digital Extremes</a>												</span>
+                        </div>
+                        <div class="whitespace-nowrap overflow-hidden overflow-ellipsis relative z-20">
+                            <span class="uppercase text-gray-400 whitespace-nowrap text-xs md:text-sm">Publisher:</span>
+                            <span class="whitespace-nowrap overflow-hidden overflow-ellipsis relative z-20">
+                                                            <a href="https://store.steampowered.com/publisher/DigitalExtremes?snr=1_4_600__629">Digital Extremes</a>												</span>
+                        </div>
+                        <div class="whitespace-nowrap overflow-hidden overflow-ellipsis relative z-20">
+                            <span class="uppercase text-gray-400 whitespace-nowrap text-xs md:text-sm">All Reviews:</span>
+                            <span class="whitespace-nowrap overflow-hidden overflow-ellipsis relative z-20">
+                                                        <span class="text-positive">
+                                                            {{$juego->costo}}	$												</span>
+                            <span class="text-gray-300"></span>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="absolute left-0  pl-7 pt-1">
+                        <button href="https://store.steampowered.com/app/230410/Warframe/" class="px-4 text-base block text-green-300 rounded-sm border-2 border-opacity-20 bg-gradient-to-b duration-700 from-green-400 to-green-800">
+                            Install now
+                        </button>
+                    </div>
+                </div>
+        </div>
+    </div>
+    @endforeach
+</section>
+
+ 
+  
+  
+
+   
+    {{-- <section class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 content-center pt-8 m-4 text-white">
 
         @foreach ($juegos as $juego)
             <div class="hover:scale-110 mb-8 bg-gray-800 rounded-lg overflow-hidden transition duration-300 ease-in-out transform hover:opacity-75">
@@ -49,7 +118,15 @@
             </div>
         @endforeach
 
+<<<<<<< Updated upstream
     </section>
+=======
+    </section> --}}
+        
+    
+    
+
+>>>>>>> Stashed changes
 
 </body>
 
