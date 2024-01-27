7cbf089a53eb085900d359c06ba5152978bbed1a
Mateo M <fmmedina3@espe.edu.ec>
2024-01-26 20:07:59 -0500

    Corrección Nav

--- resources/views/vistas/nav.blade.php
+++ resources/views/vistas/nav.blade.php
@@ -1,25 +1,21 @@
-<header class="absolute text-gray-400 bg-transparent body-font w-full h-full md:h-0">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+<header class="absolute w-full h-full text-gray-400 bg-transparent body-font md:h-0">
+    <div class="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
         <a>
             <img class="size-28" src="{{ asset('img/logoGamerFest.png') }}" alt="GamerFest">
         </a>
         <nav
-            class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
-            <a class="mr-5 hover:text-purple-600 hover:underline font-bold" href="#juegos">Juegos</a>
-            <a class="mr-5 hover:text-purple-600 hover:underline font-bold" href="#sobrenosotros">Sobre Nosotros</a>
+            class="flex flex-wrap items-center justify-center text-base md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700">
+            <a class="mr-5 font-bold hover:text-purple-600 hover:underline" href="#juegos">Juegos</a>
+            <a class="mr-5 font-bold hover:text-purple-600 hover:underline" href="#sobrenosotros">Sobre Nosotros</a>
         </nav>
         <a href="{{ route('login') }}">
             <button
-                class="block w-1/2 mx-1 text-center text-lg bg-purple-800 hover:bg-purple-600 my-2 py-2 px-4 rounded-full transition-all duration-300 border-2 border-transparent hover:border-white focus:outline-none focus:ring focus:border-purple-300 font-medium text-white leading-5 md:mx-2 md:w-auto ">Ingresar
+                class="block w-1/2 px-4 py-2 mx-1 my-2 text-lg font-medium leading-5 text-center text-white transition-all duration-300 bg-purple-800 border-2 border-transparent rounded-full hover:bg-purple-600 hover:border-white focus:outline-none focus:ring focus:border-purple-300 md:mx-2 md:w-auto ">Ingresar
             </button>
         </a>
         <a href="{{ route('register') }}">
             <button
-                class="block w-1/2 mx-1 text-center text-lg bg-purple-800 hover:bg-purple-600 my-2 py-2 px-4 rounded-full transition-all duration-300 border-2 border-transparent hover:border-white focus:outline-none focus:ring focus:border-purple-300 font-medium text-white leading-5 md:mx-0 md:w-auto">Registrarse</button>
+                class="block w-1/2 px-4 py-2 mx-1 my-2 text-lg font-medium leading-5 text-center text-white transition-all duration-300 bg-purple-800 border-2 border-transparent rounded-full hover:bg-purple-600 hover:border-white focus:outline-none focus:ring focus:border-purple-300 md:mx-0 md:w-auto">Registrarse</button>
         </a>
     </div>
-
-    <button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">...</button>
-
-
 </header>
