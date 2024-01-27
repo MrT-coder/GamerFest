03847ba9dbd3ce6a470246bbcbf923fb0c090208
Mateo M. <fmmedina3@espe.edu.ec>
2024-01-27 15:22:03 -0500

    Corrección de estilo en sección contador

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,8 +61,7 @@
                     Lista de Juegos
                 </h1>
                 <p class="w-full text-lg leading-relaxed lg:w-1/2 text-opacity-80">
-                    A continuación encontrarás la lista de juegos a los que puedes inscribirte. ¡No te quedes sin tu
-                    cupo!
+                    A continuación encontrarás la lista de juegos a los que puedes inscribirte. ¡No te quedes sin jugar!
                 </p>
             </div>
             <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
@@ -104,87 +103,101 @@
             </a>
     </section>
 
-    {{-- Contador --}}
     <section id="contador">
-        <div class="w-50 h-50 bg-gradient-to-r from-blue-400 via-rose-900 to-indigo-500">
-            <div class="bg-black w-50 h-50 bg-opacity-70">
-                <div
-                    class="container flex flex-col items-start justify-between w-full h-full px-8 py-8 mx-auto lg:px-4 xl:px-0">
-                    <img src="{{ asset('img/logoGamerFest.png') }}" alt="" class="flex-initial">
-                    <div class="flex flex-col items-start justify-center flex-1">
-
-                        <h1
-                            class="mt-12 font-serif text-6xl font-bold tracking-wider text-center text-gray-200 lg:text-7xl xl:text-8xl md:text-left">
-                            Pronto iniciara el <span class="text-yellow-300">GamerFest</span> </h1>
-                        <div class="flex flex-col items-center mt-8 mt-12 ml-2">
-                            <p class="text-sm text-gray-300 uppercase">Tiempo restante para la emoción </p>
-                            <div class="flex items-center justify-center mt-4 space-x-4" x-data="timer(new Date().setDate(new Date().getDate() + 1))"
-                                x-init="init();">
-                                <div class="flex flex-col items-center px-4">
-                                    <span id="days" class="text-4xl text-gray-200 lg:text-5xl">00</span>
-                                    <span class="mt-2 text-gray-400">Dias</span>
-                                </div>
-                                <span class="w-[1px] h-24 bg-gray-400"></span>
-                                <div class="flex flex-col items-center px-4">
-                                    <span id="hours" class="text-4xl text-gray-200 lg:text-5xl">00</span>
-                                    <span class="mt-2 text-gray-400">Horas</span>
-                                </div>
-                                <span class="w-[1px] h-24 bg-gray-400"></span>
-                                <div class="flex flex-col items-center px-4">
-                                    <span id="minutes" class="text-4xl text-gray-200 lg:text-5xl">00</span>
-                                    <span class="mt-2 text-gray-400">Minutos</span>
-                                </div>
-                                <span class="w-[1px] h-24 bg-gray-400"></span>
-                                <div class="flex flex-col items-center px-4">
-                                    <span id="seconds" class="text-4xl text-gray-200 lg:text-5xl">00</span>
-                                    <span class="mt-2 text-gray-400">Segundos</span>
+        <div class="bg-gradient-to-r from-blue-400 via-rose-900 to-indigo-500">
+            <div class="bg-black bg-opacity-70">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex flex-col items-center">
+                        <img src="{{ asset('img/logoGamerFest.png') }}" alt="Logo GamerFest" class="max-h-32">
+                        <div class="flex flex-col">
+                            <h1 class="mt-5 text-3xl font-bold text-center text-white md:text-7xl">
+                                Pronto iniciará el<br><span class="text-yellow-300">GamerFest</span>
+                            </h1>
+                            <div class="flex flex-col items-center mt-12 ml-2">
+                                <p class="text-sm text-gray-300 uppercase">
+                                    Tiempo restante para la emoción
+                                </p>
+                                <div class="flex flex-col items-center justify-center mt-4 md:flex-row"
+                                    x-data="timer(new Date().setDate(new Date().getDate() + 1))" x-init="init();">
+                                    <div class="flex flex-col items-center px-4">
+                                        <span id="days" class="text-4xl text-gray-200 lg:text-5xl">
+                                            00
+                                        </span>
+                                        <span class="mt-2 text-gray-400">
+                                            Dias
+                                        </span>
+                                    </div>
+                                    <span class="h-[1px] w-[100px] my-1 md:w-[1px] md:h-24 bg-gray-400"></span>
+                                    <div class="flex flex-col items-center px-4">
+                                        <span id="hours" class="text-4xl text-gray-200 lg:text-5xl">
+                                            00
+                                        </span>
+                                        <span class="mt-2 text-gray-400">
+                                            Horas
+                                        </span>
+                                    </div>
+                                    <span class="h-[1px] w-[100px] my-1 md:w-[1px] md:h-24 bg-gray-400"></span>
+                                    <div class="flex flex-col items-center px-4">
+                                        <span id="minutes" class="text-4xl text-gray-200 lg:text-5xl">
+                                            00
+                                        </span>
+                                        <span class="mt-2 text-gray-400">
+                                            Minutos
+                                        </span>
+                                    </div>
+                                    <span class="h-[1px] w-[100px] my-1 md:w-[1px] md:h-24 bg-gray-400"></span>
+                                    <div class="flex flex-col items-center px-4">
+                                        <span id="seconds" class="text-4xl text-gray-200 lg:text-5xl">
+                                            00
+                                        </span>
+                                        <span class="mt-2 text-gray-400">
+                                            Segundos
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <script>
+                    const $days = document.getElementById('days'),
+                        $hours = document.getElementById('hours'),
+                        $minutes = document.getElementById('minutes'),
+                        $seconds = document.getElementById('seconds'),
+                        $finalMessage = document.querySelector('.final-sms');
 
-                    <script>
-                        const $days = document.getElementById('days'),
-                            $hours = document.getElementById('hours'),
-                            $minutes = document.getElementById('minutes'),
-                            $seconds = document.getElementById('seconds'),
-                            $finalMessage = document.querySelector('.final-sms');
+                    //Fecha a futuro
+                    const countdownDate = new Date('03 30, 2024 00:00:00').getTime();
 
-                        //Fecha a futuro
-                        const countdownDate = new Date('01 30, 2024 00:00:00').getTime();
+                    let interval = setInterval(function() {
+                        //Obtener fecha actual y milisegundos
+                        const now = new Date().getTime();
 
-                        let interval = setInterval(function() {
-                            //Obtener fecha actual y milisegundos
-                            const now = new Date().getTime();
+                        //Obtener las distancias entre ambas fechas
+                        let distance = countdownDate - now;
 
-                            //Obtener las distancias entre ambas fechas
-                            let distance = countdownDate - now;
+                        //Calculos a dias-horas-minutos-segundos
+                        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                        let seconds = Math.floor((distance % (1000 * 60)) / (1000));
 
-                            //Calculos a dias-horas-minutos-segundos
-                            let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                            let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                            let seconds = Math.floor((distance % (1000 * 60)) / (1000));
+                        //Escribimos resultados
+                        $days.innerHTML = days;
+                        $hours.innerHTML = hours;
+                        $minutes.innerHTML = minutes;
+                        $seconds.innerHTML = ('0' + seconds).slice(-2);
 
-                            //Escribimos resultados
-                            $days.innerHTML = days;
-                            $hours.innerHTML = hours;
-                            $minutes.innerHTML = minutes;
-                            $seconds.innerHTML = ('0' + seconds).slice(-2);
-
-                            //Cuando llegue a 0
-                            if (distance < 0) {
-                                clearInterval(interval);
-                                $finalMessage.style.transform = 'translateY(0)';
-                            }
-                        }, 1000);
-                    </script>
-
+                        //Cuando llegue a 0
+                        if (distance < 0) {
+                            clearInterval(interval);
+                            $finalMessage.style.transform = 'translateY(0)';
+                        }
+                    }, 1000);
+                </script>
     </section>
 
     {{-- Footer --}}
-
-    <!-- component -->
     <footer class="relative flex flex-col items-center h-screen p-8 overflow-hidden text-white bg-cyan-900 md:py-40"
         id="sobrenosotros">
         <div class="relative z-[1] container m-auto px-6 md:px-12 text-lg items-center">
@@ -223,49 +236,6 @@
 
                     </div>
                 </div>
-            </div>
-        </div>
-        <div aria-hidden="true" class="absolute inset-0 flex items-center h-screen">
-            <div aria-hidden="true" class="bg-layers bg-scale w-56 h-56 m-auto blur-xl bg-url['{
-  "
-                type": "selection" , "guid" : "1f7140a7" , "source" : "a3fbc0074" , "data" : { "nodes" : [ { "id"
-                : "6b209ba7" , "x" : 112, "height" : 39, "y" : 84, "width" : 349, "rotation" : 0, "transform"
-                : "matrix(1 0 0 1 112 84)" , "inspectables" : { "width_policy" : "auto" , "height_policy" : "auto"
-                , "color" : "rgb(68, 68, 68)" , "font_size" : 30, "text_align" : "left" , "line_height" : 1.3, "bold" :
-                null, "italic" : false, "underline" : false, "opacity" : 100, "strikethrough" : false, "small_caps" :
-                false, "uppercase" : false, "letter_spacing" : 0, "font" : "Source Sans Pro" , "font_weight" :
-                600, "aspect_lock" : false, "fe_dropshadow_enabled" : false, "fe_dropshadow_opacity" :
-                75, "fe_dropshadow_angle" : 90, "fe_dropshadow_distance" : 5, "fe_dropshadow_size" :
-                5, "fe_dropshadow_color" : "rgb(0, 0, 0)" , "vertical_align" : "top" , "padding_left" :
-                0, "padding_right" : 0, "padding_top" : 0, "padding_bottom" : 0, "fe_blur_enabled" :
-                false, "fe_blur_size" : 5 }, "text" : "<p dir=\" auto\">N
-                DE PERSONAS INSCRITAS</p>",
-                "name": "text",
-                "link": null,
-                "deps": {}
-                }
-                ],
-                "hierarchy": {
-                "6b209ba7": {
-                "id": "6b209ba7",
-                "type": "item",
-                "locked": false,
-                "link": null,
-                "aspect_lock": false,
-                "visible": true,
-                "instance_name": "Paragraph 3 Copy 25"
-                }
-                },
-                "idMapping": {
-                "91a2ba5b": "6b209ba7"
-                },
-                "size": {
-                "width": 349,
-                "height": 39
-                },
-                "symbolInstances": {}
-                }
-                }'] rounded-full md:w-[30rem] md:h-[30rem] md:blur-3xl">
             </div>
         </div>
         <div aria-hidden="true" class="absolute inset-0 w-screen h-screen opacity-80"></div>
